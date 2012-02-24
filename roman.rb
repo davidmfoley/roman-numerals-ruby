d1904d154f4f1b823f19e878cce52a28fe47e317
Dave Foley <davidmfoley@gmail.com>
2012-02-24 10:43:30 -0800

    passing

--- roman.rb
+++ roman.rb
@@ -1,8 +1,13 @@
 require 'minitest/unit'
 require 'minitest/autorun'
+class Fixnum
+  def to_roman
+    'I'
+  end
+end
 class RomanNumeralsTests < MiniTest::Unit::TestCase
-  def test_hello
-
+  def test_i
+    assert_equal('I', 1.to_roman)
   end
 end
 
