7cd76c43c89d15cf493841934980e678856da281
Dave Foley <davidmfoley@gmail.com>
2014-01-18 10:09:11 -0800

    clean up

--- roman.rb
+++ roman.rb
@@ -2,46 +2,54 @@ require 'minitest/unit'
 require 'minitest/autorun'
 class Fixnum
   def to_roman
-    fifties = self / 50
-    tens = self / 10 % 5
-    fives = self % 10 / 5
-    ones = self % 5
-    naive = ('L' * fifties) + ('X' * tens) + ('V' * fives) + ('I' * ones)
-    naive = naive.gsub('IIII', 'IV')
-    naive = naive.gsub('VIV', 'IX')
+    vals = {
+      'M' => 1000,
+      'D' => 500,
+      'C' => 100,
+      'L' => 50,
+      'X' => 10,
+      'V' => 5,
+      'I' => 1,
+    }
+    remainder = self
+    roman = ""
+
+    vals.each do |letter, divisor|
+      count = remainder / divisor
+      roman += letter * count
+      remainder = remainder % divisor
+    end
+
+    roman = roman.gsub('IIII', 'IV')
+    roman = roman.gsub('CCCC', 'CD')
+    roman = roman.gsub('VIV', 'IX')
+    roman = roman.gsub('DCD', 'CM')
   end
 end
 
+
 class RomanNumeralsTests < MiniTest::Unit::TestCase
-  def test_i
-    assert_equal('I', 1.to_roman)
+  def self.test roman, numeric
+    define_method "test_#{roman}" do
+      assert_equal(roman, numeric.to_roman)
+    end
   end
-  def test_ii
-    assert_equal('II', 2.to_roman)
-  end
-  def test_v
-    assert_equal('V', 5.to_roman)
-  end
-  def test_x
-    assert_equal('X', 10.to_roman)
-  end
-  def test_iv
-    assert_equal('IV', 4.to_roman)
-  end
-  def test_xiv
-    assert_equal('XIV', 14.to_roman)
-  end
-  def test_ix
-    assert_equal('IX', 9.to_roman)
-  end
-  def test_xx
-    assert_equal('XX', 20.to_roman)
-  end
-  def test_xvi
-    assert_equal('XVI', 16.to_roman)
-  end
-  def test_lviii
-    assert_equal('LVIII', 58.to_roman)
-  end
+
+  test('I', 1)
+  test('II', 2)
+  test('V', 5)
+  test('X', 10)
+  test('IV', 4)
+  test('XIV', 14)
+  test('IX', 9)
+  test('XX', 20)
+  test('XVI', 16)
+  test('LVIII', 58)
+  test('CC', 200)
+  test('MM', 2000)
+  test('MDLVI', 1556)
+  test('MCD', 1400)
+  test('MCM', 1900)
+  test('MCM', 1900)
 end
 
